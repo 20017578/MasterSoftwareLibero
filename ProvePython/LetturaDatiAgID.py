2c3146f5f06e6a96888bc3ea3bf00ab2df24cecf
20017578 <20017578@studenti.uniupo.it>
2016-02-27 12:40:38 +0100

    Creato un "dizionario" con le scuole per ogni comune

--- ProvePython/LetturaDatiAgID.py
+++ ProvePython/LetturaDatiAgID.py
@@ -25,7 +25,7 @@ except:
         print 'Ho scaricato i dati ', fonteDati, ' da ', URL_Dati
     except:
         # Se non siamo riusciti, forse serve impostare il proxy della della Regione
-        print '... provo col proxy'
+        print '... provo col proxy della Regione'
         proxies = {
             'http': 'http://10.102.162.8:80',
             'https': 'http://10.102.162.8:80',
@@ -44,8 +44,11 @@ print len (grafo_AgID)
 URI_amministrazione = rdflib.URIRef ("http://spcdata.digitpa.gov.it/Amministrazione")
 URI_pec = rdflib.URIRef ('http://spcdata.digitpa.gov.it/PEC')
 URI_mail = rdflib.URIRef ('http://xmlns.com/foaf/0.1/mbox')
+URI_locatedIn = rdflib.URIRef ('http://www.geonames.org/ontology#locatedIn')
+URI_label = rdflib.URIRef ('http://www.w3.org/2000/01/rdf-schema#label')
 
 listaMeccanograficiAgID = []
+listaScuolePerComune = {}
 conto = 0
 for amministrazione in grafo_AgID.subjects (predicate=rdflib.RDF.type, object=URI_amministrazione):
     meccanograficoScuola = ''
@@ -63,6 +66,26 @@ for amministrazione in grafo_AgID.subjects (predicate=rdflib.RDF.type, object=UR
     if meccanograficoScuola != '':
         if len(meccanograficoScuola) != 10:
             print "%s sembra una scuola, ma il codice %s non sembra un meccanografico"%(str(amministrazione), meccanograficoScuola)
+        else:
+            for Comune in grafo_AgID.objects(amministrazione,URI_locatedIn):
+                codiceCatastaleComune = str(Comune).split('/')[-1];
+                if codiceCatastaleComune in listaScuolePerComune:
+                    listaScuolePerComune[codiceCatastaleComune].append(meccanograficoScuola.upper ())
+                else:
+                    listaScuolePerComune[codiceCatastaleComune] = [meccanograficoScuola.upper ()]
         listaMeccanograficiAgID.append (meccanograficoScuola.upper ())
+
+print 'Su ', conto, ' amministrazioni, ho trovato', len (listaMeccanograficiAgID), ' presunte scuole, distribuite su ', len (listaScuolePerComune), 'comuni'
+
+print "Stampo quello che so sull'ultima scuola trovata."
+
+for p,o in grafo_AgID.predicate_objects(unaScuola):
+    print str(p), str(o)
+    for lab in grafo_AgID.objects(o,URI_label):
+        print ':::: label', str(lab)
+
+print 'Lista dei comuni', listaScuolePerComune.keys()
+
+print 'Lista delle ', len(listaScuolePerComune['L219']), 'scuole trovate nel comune di Torino', listaScuolePerComune['L219']
 
 grafo_AgID = ''
