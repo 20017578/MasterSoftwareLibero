aaa395154ce4fc9ce4e08f77bf833ef26c871f2a
20017578 <20017578@studenti.uniupo.it>
2016-04-12 08:30:50 +0200

    Generazione grafo da dati MIUR, per ora 197237 terne

--- ProvePython/MIUR_csv2LOD.py
+++ ProvePython/MIUR_csv2LOD.py
@@ -1,6 +1,6 @@
 #
 # Su Ubuntu/Debian i pacchetti necessari si installano con:
-# apt-get install python-rdflib python-requests
+# apt-get install python-rdflib
 #
 
 import rdflib
@@ -16,7 +16,7 @@ fonteDati = 'MIUR'
 nomeFileDati = 'DatiMIUR.csv'
 separatoreDati = ';'
 try:
-    letturaRighe = csv.DictReader (open (nomeFileDati), delimiter = separatoreDati)
+    csvDatiMIUR = csv.DictReader (open (nomeFileDati), delimiter = separatoreDati)
     print 'Ho letto i dati', fonteDati, 'dal file', nomeFileDati
 except:
     print 'File', nomeFileDati, 'non trovato, lanciare ScaricaDatiDaRete.py'
@@ -47,5 +47,28 @@ if namespace_MIUR == None:
     raise ValueError('namespace non trovato')
 
 namespace_scuole = namespace_MIUR + 'Scuola/'
+namespace_ontologia = namespace_MIUR + 'ontologia#'
 
+caratteristiche = set ()
+tipiIstituzione = set ()
 
+for rigaScuola in csvDatiMIUR:
+    meccanografico = rigaScuola['PLESSO/SCUOLA'].upper ()
+    grafo_MIUR.add ( (namespace_scuole + meccanografico, namespace_ontologia + 'meccanografico', rdflib.Literal (meccanografico)) )
+    autonomia = rigaScuola['ISTITUTO PRINCIPALE'].upper ()
+    if autonomia == meccanografico:
+        if rigaScuola['DENOMINAZIONE'].upper () != rigaScuola['DENOMINAZIONE ISTITUTO PRINCIPALE'].upper ():
+            print 'Anomalia sulla autonomia', meccanografico, 'denominazioni non coincidenti'
+        grafo_MIUR.add ( (namespace_scuole + meccanografico, rdflib.RDF.type, namespace_ontologia + 'Autonomia') )
+    else:
+        grafo_MIUR.add ( (namespace_scuole + meccanografico, rdflib.RDF.type, namespace_ontologia + 'PuntoErogazioneServizio') )
+        grafo_MIUR.add ( (namespace_scuole + meccanografico, namespace_ontologia + 'haIstitutoPrincipale', namespace_scuole + autonomia) )
+    grafo_MIUR.add ( (namespace_scuole + meccanografico, rdflib.RDFS.label, rdflib.Literal (rigaScuola['DENOMINAZIONE'])) )
+    caratteristiche |= {rigaScuola['CARATTERISTICA']}
+    tipiIstituzione |= {rigaScuola['TIPO ISTITUZIONE']}
+
+grafo_MIUR.serialize (destination=open ('MIUR.ttl', 'w'), format=formatoDati)
+print 'Scritto un grafo con', len (grafo_MIUR), 'terne.'
+
+print 'Caratteristiche trovate:', caratteristiche
+print 'Valori per TIPO ISTITUZIONE:', tipiIstituzione
