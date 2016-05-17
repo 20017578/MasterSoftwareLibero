dcdc8c2daaa8f98d98a09fb225663d7395d0344f
guido612 <guido.vernero@gmail.com>
2016-05-17 19:16:01 +0200

    cambiamentocsv di configurazione

--- ProvePython/Trasparenza/leggi_config.py
+++ ProvePython/Trasparenza/leggi_config.py
@@ -6,23 +6,13 @@ import sys
 import csv
 import string
 
-# FUNZIONE PER LA PULIZIA DELLA STRINGA IN INPUT
-def funzione(stringa_pa):
-
-    return stringa_pulita
-
-
-
-
-
-
 try:
     file_csv_config_main = open ('campi_config.csv')
 except:
     print 'File non trovato, provo da rete.'
 
 def_campi=[]
-# LEGGIAMO IL FILE CON IL NOME DEI TIPI DI CAMPO
+# LEGGIAMO IL FILE CON IL NOME DEI TIPI DI CAMPO (Attenzione le prime 10 righe descrivono il nodo e non il campo
 riga_csv=file_csv_config_main.readline()
 while riga_csv:
     riga_csv=riga_csv.rstrip()
@@ -39,11 +29,11 @@ with open(nomeFileDati, 'rb') as csvfile:
        for row in reader:
            n_righe=n_righe+1
            n_campi=len(row)
-           i=0
-           while i<n_campi:
-              campi.append(row[i])
-              i=i+1
-
+           if n_righe>9:
+                i = 0
+                while i<n_campi:
+                    campi.append(row[i])
+                    i=i+1
 
 print ('Il CSV da sottoporre deve avere le seguenti caratteristiche :')
 print ('NUMERO CAMPI POSSIBILI = ',n_campi)
@@ -52,7 +42,7 @@ while i<n_campi:
     print
     print 'Campo nr.',i+1,' :'
     j=0
-    while j<n_righe:
-        print def_campi[j],' -> ',campi[(j*n_campi)+i]
+    while j<n_righe-10:
+        print (j*n_campi)+i,':',def_campi[j+10],' -> ',campi[(j*n_campi)+i]
         j=j+1
     i=i+1
